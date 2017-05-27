5cadea7b1e90e1844df246596f14a829e064e321
Krzysztof Jurkiewicz <huntekah@gmail.com>
2017-05-27 18:03:34 +0200

    class picture complete

--- DecisionModel.py
+++ DecisionModel.py
@@ -2,13 +2,14 @@ from sklearn import tree
 import numpy as np
 import pickle
 from skimage import io
+from vessel_detect.picture import Picture
 
 
 def get_image(path, asgrey=True, _flatten=False):
     return io.imread(path, as_grey=asgrey, flatten=_flatten)
 
 
-class cart:
+class Cart:
     '''we use it to create model, that will predict if a pixel is a vessel'''
 
     def __init__(self, depth):
@@ -20,38 +21,78 @@ class cart:
         pickle.dump(self.clf, file)
 
 
-class picture_decision:
-    def __init__(self, input_file, output_file, size=5):
-        self.input = get_image(input_file, asgrey=False)
-        self.output = get_image(output_file, asgrey=False)
-        self.size = size
-        self.decision_X = []
-        self.decision_Y = []
+class Picture_decision:
+    '''Usedto create a decision model'''
 
-    def set_size(self, size):
-        self.size = size
+    def __init__(self, input_file, output_file, tile_size=5):
+        # self.input = get_image(input_file, asgrey=False)
+        # self.output = get_image(output_file, asgrey=False)
+        self.picture = Picture(input_file, output_file, tile_size=tile_size)
+        self.tile_size = tile_size
+        self.decision_X = [[], [], [], [], [], [], []]
+        self.decision_Y = [[], [], [], [], [], [], []]
+
+    def set_tile_size(self, size):
+        self.tile_size = size
+
+    def clear_decisions(self):
+        self.decision_X = [[], [], [], [], [], [], []]
+        self.decision_Y = [[], [], [], [], [], [], []]
 
     def build_decision_model(self):
         # several decision models
-        self.pixel_model()
+        self.pixel_color_model(0)
+        self.pixel_average_model(1)
+        self.tile_average_model(2)
 
-    def pixel_model(self):
-        self.decision_X.append([])
-        self.decision_Y.append([])
-        scale = len(self.output) / len(self.input)
-        for i, row in enumerate(self.input):
+    def set_picture(self, input_file, output_file):
+        self.picture = Picture(input_file, output_file, tile_size=self.tile_size)
+
+    def pixel_color_model(self, index=0):
+        '''Based on the pixel color choose output 0/1'''
+        for i, row in enumerate(self.picture.input):
             for j, pixel in enumerate(row):
-                self.decision_X.append(pixel)
-                self.decision_Y.append(self.output[i*scale][j*scale])
+                self.decision_X[index].append(pixel)
+                # one output not multiple!
+                self.decision_Y[index].append(self.picture.output[i][j])
 
-    def pixel_average_model(self):
-        average_color_per_row = np.average(self.input, axis=0)
+    def pixel_average_model(self, index=1):
+        average_color_per_row = np.average(self.picture.input, axis=0)
         self.average_color = np.average(average_color_per_row, axis=0)
-        self.decision_X.append([])
-        self.decision_Y.append([])
-        scale = len(self.output) / len(self.input)
-        for i, row in enumerate(self.input):
+        # self.decision_X.append([])
+        # self.decision_Y.append([])
+        for i, row in enumerate(self.picture.input):
             for j, pixel in enumerate(row):
                 decision_x = [pixel, self.average_color]
-                self.decision_X.append(decision_x)
-                self.decision_Y.append(self.output[i * scale][j * scale])
+                self.decision_X[index].append(decision_x)
+                self.decision_Y[index].append(self.picture.output[i][j])
+
+    def tile_average_model(self, index=2):
+        average_color_per_row = np.average(self.picture.input, axis=0)
+        self.average_color = np.average(average_color_per_row, axis=0)
+        tile = self.picture.next_tile()
+        while tile:
+            average_color_per_row = np.average(tile.input, axis=0)
+            average_color_per_tile = np.average(average_color_per_row, axis=0)
+            decision_x = [tile.input_pixel, average_color_per_tile, self.average_color]
+            self.decision_X[index].append(decision_x)
+            self.decision_Y[index].append(tile.output)
+            tile = self.picture.next_tile()
+
+
+if __name__ == "__main__":
+    # decisions = Picture_decision("Test/input5x5.ppm", "Test/mask5x5.ppm", tile_size=3)
+    # decisions.tile_average_model()
+    # print(decisions.decision_X)
+    # print("\n")
+    # print(decisions.decision_Y)
+    a = []
+    a.extend([[], [], [], [], [], []])
+    b = []
+    b.append([])
+    b.append([])
+    b.append([])
+    c = [[], [], [], [], [], []]
+    print(a)
+    print(b)
+    print(c)
---
+++ picture.py
@@ -0,0 +1,100 @@
+from skimage import io
+import numpy as np
+
+
+class Picture:
+    def __init__(self, path_input, path_output=None, tile_size=5):
+        self.load_image(path_input, path_output)
+        self.shape = self.input.shape
+        self.current_tile = 0
+        self.tile_size = tile_size
+
+    def tile(self, n=None):
+        """ n represents nth tile of size tile_zize square """
+        if n:
+            self.current_tile = n
+        else:
+            n = self.current_tile
+        x = int(n % (self.shape[0] - self.tile_size + 1))
+        y = int(np.floor(n / (self.shape[0] - self.tile_size + 1)))
+        y = y % (self.shape[1] - self.tile_size + 1)
+
+        class Tile:
+            def __init__(self):
+                pass
+
+            def set_input(self, tile):
+                self.input = tile
+                len = tile.shape[0]
+                self.input_pixel = tile[int((len-1)/2)][int((len-1)/2)]
+
+            def set_red(self, tile):
+                self.red = tile
+
+            def set_green(self, tile):
+                self.green = tile
+
+            def set_blue(self, tile):
+                self.blue = tile
+
+            def set_grey(self, tile):
+                self.grey = tile
+
+            def set_output(self, pixel):
+                self.output = pixel
+
+            def __bool__(self):
+                return True
+
+        result = Tile()
+        result.set_input(self.input[x:(x + self.tile_size), y:(y + self.tile_size), :])
+        result.set_red(self.red[x:x + self.tile_size, y:y + self.tile_size])
+        result.set_green(self.green[x:x + self.tile_size, y:y + self.tile_size])
+        result.set_blue(self.blue[x:x + self.tile_size, y:y + self.tile_size])
+        result.set_grey(self.grey[x:x + self.tile_size, y:y + self.tile_size])
+        if hasattr(self, 'output'):
+            result.set_output(self.output[x + int((self.tile_size - 1) / 2), y + int((self.tile_size - 1) / 2)])
+            print(x + int((self.tile_size - 1) / 2), y + int((self.tile_size - 1) / 2))
+        return result
+
+    def next_tile(self):
+        if self.current_tile >= (self.shape[0] - self.tile_size + 1) * (self.shape[1] - self.tile_size + 1):
+            return False
+        result = self.tile()
+        self.current_tile += 1
+        return result
+
+    def load_image(self, path_input, path_output=None):
+        self.input = self.get_image(path_input, asgrey=False)
+        self.red = self.input[:, :, 0]
+        self.green = self.input[:, :, 1]
+        self.blue = self.input[:, :, 2]
+        self.grey = self.get_image(path_input, asgrey=True, _flatten=True)
+        if path_output:
+            self.output = self.get_image(path_output, asgrey=True, _flatten=True)
+
+    def get_image(self, path, asgrey=True, _flatten=False):
+        return io.imread(path, as_grey=asgrey, flatten=_flatten)
+
+
+if __name__ == "__main__":
+    # image = picture("Test/input/im21.ppm", "Test/output/m_im21.ppm")
+    image = Picture("Test/input3x3.ppm", "Test/mask3x3.ppm", tile_size=3)
+    tile = image.tile()
+    i = 0
+    while tile:
+        print(i, tile.output)
+        i += 1
+        tile = image.next_tile()
+        # print("input\n")
+        # print(tile.input)
+        # print("red\n")
+        # print(tile.red)
+        # print("green\n")
+        # print(tile.green)
+        # print("blue\n")
+        # print(tile.blue)
+        # print("grey\n")
+        # print(tile.grey)
+        # print("output\n")
+        # print(tile.output)
